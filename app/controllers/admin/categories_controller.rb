0ffe46f436707513ee24dad41cde12896298df57
jeffreypang911 <jeffreypang911@gmail.com>
2018-10-11 19:15:34 +0000

    adds feature for admin category creation

---
+++ app/controllers/admin/categories_controller.rb
@@ -0,0 +1,26 @@
+class Admin::CategoriesController < ApplicationController
+    def index
+        @categories = Category.all
+    end
+
+    def new
+        @category = Category.new
+    end
+
+    def create
+        @categories = Category.new(catagory_params)
+
+        if @categories.save
+            redirect_to [:admin, :categories], notice: 'Product created!'
+          else
+            render :new
+          end
+    end
+
+    def catagory_params
+        params.require(:category).permit(
+          :name
+        )
+    end
+end
+  
